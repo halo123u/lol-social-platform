d21dfb8c2ba8c748ee1754a544f13fa27c2a1cd9
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-10-14 21:04:14 -0400

    add POST /users/login

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,5 +2,7 @@ const userRoutes = require('express').Router(),
 userController = require('../controllers/userController');
 
 userRoutes.post('/', userController.newUser);
+userRoutes.post('/login', userController.signIn);
+
 
 module.exports = userRoutes; 
